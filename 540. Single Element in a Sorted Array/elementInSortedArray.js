b4bcd84a0748d3dc2002e66806e3d7bb6a3d8f68
anubhavkhanna7 <anubhavkhanna7@gmail.com>
2024-09-07 16:58:46 +0530

    More Questions Solved

--- 540. Single Element in a Sorted Array/elementInSortedArray.js
+++ 540. Single Element in a Sorted Array/elementInSortedArray.js
@@ -18,3 +18,24 @@ var singleNonDuplicate = function(nums) {
    
    return nums[start];
 };
+
+var singleNonDuplicate = function(nums) {
+    let start = 0, end = nums.length-1
+ 
+     while (start <= end) {
+         let mid = Math.floor((start + end)/2)
+ 
+         if (mid ==0 && nums[mid] !== nums[mid+1]) return nums[mid]
+         if (mid == nums.length-1 && nums[mid] !== nums[mid-1]) return nums[mid]
+         if (nums[mid] !== nums[mid+1] && nums[mid] !== nums[mid-1]) return nums[mid]
+ 
+ 
+         if (mid%2 === 0 && nums[mid+1] === nums[mid]) {
+             start = mid+1
+         } else if (mid%2 === 1 && nums[mid-1] === nums[mid]) {
+             start = mid + 1
+         } else {
+             end = mid-1
+         }
+     } 
+ };
