ed77fdc6a7b90b534ad570113f8190b45d6ef270
anubhavkhanna7 <anubhavkhanna7@gmail.com>
2024-09-09 02:54:02 +0530

    More Questions Solved

---
+++ 1526. Minimum Number of Increments on Subarrays to Form a Target Array/minNumberIncrements.js
@@ -0,0 +1,9 @@
+var minNumberOperations = function(target) {
+  let operations = target[0];
+  for (let i = 1; i < target.length; i++) {
+      if (target[i] > target[i - 1]) {
+          operations += target[i] - target[i - 1];
+      }
+  }
+  return operations;
+};
