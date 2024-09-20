e30102f6e41ce34c048537f26e4d4ff6db4066c4
anubhavkhanna7 <anubhavkhanna7@gmail.com>
2024-09-20 21:37:20 +0530

    More Questions Solved

---
+++ 287. Find the Duplicate Number/duplicate.js
@@ -0,0 +1,16 @@
+var findDuplicate = function(nums) {
+  let fast = nums[0], slow = nums[0]
+
+  do {
+      slow = nums[slow]
+      fast = nums[nums[fast]]
+  } while(slow !== fast)
+
+  fast = nums[0]
+  while(fast!== slow) {
+      fast = nums[fast]
+      slow = nums[slow]
+  }
+
+  return slow
+};
