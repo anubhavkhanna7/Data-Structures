19d7383b5b6f814741286bbbbe0f1aeb35809a0d
anubhavkhanna7 <anubhavkhanna7@gmail.com>
2024-09-22 12:34:46 +0530

    More Questions Solved

--- 1137. N-th Tribonacci Number/tribo.js
+++ 1137. N-th Tribonacci Number/tribo.js
@@ -5,3 +5,16 @@ var tribonacci = function(n, memo = {}) {
   memo[n] = tribonacci(n-1, memo) + tribonacci(n-2, memo) + tribonacci(n-3, memo)
   return memo[n]
 };
+
+var tribonacci = function(n, memo = {}) {
+  if (n< 3) return n === 0 ? 0 : 1
+  let arr = new Array(n+1).fill(0)
+  arr[1] = 1
+  arr[2] = 1
+  
+  for (let i = 3; i<=n; i++) {
+      arr[i] = arr[i-1] + arr[i-2] + arr[i-3]
+  }
+  
+  return arr[arr.length-1]
+};
