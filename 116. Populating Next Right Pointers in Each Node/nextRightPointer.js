aebd25342a056817748f7f8cb1ca48281ff6fc59
anubhavkhanna7 <anubhavkhanna7@gmail.com>
2024-09-25 03:55:21 +0530

    More Questions Solved

--- 116. Populating Next Right Pointers in Each Node/nextRightPointer.js
+++ 116. Populating Next Right Pointers in Each Node/nextRightPointer.js
@@ -1,21 +1,50 @@
-var connect = function(root) {
-  if (!root) return root
+var connect = function (root) {
+    if (!root) return root
 
-  let queue = []
-  queue.push([root, 0])
+    let queue = []
+    queue.push([root, 0])
 
-  while(queue.length) {
-      let [node, level] = queue.shift()
+    while (queue.length) {
+        let [node, level] = queue.shift()
 
-      if (queue.length === 0 || queue[0][1] !== level) {
-          node.next = null
-      } else {
-          node.next = queue[0][0]
-      }
+        if (queue.length === 0 || queue[0][1] !== level) {
+            node.next = null
+        } else {
+            node.next = queue[0][0]
+        }
 
-      if (node.left) queue.push([node.left, level+1])
-      if (node.right) queue.push([node.right, level+1])
-  }
+        if (node.left) queue.push([node.left, level + 1])
+        if (node.right) queue.push([node.right, level + 1])
+    }
 
-  return root
+    return root
 };
+
+
+var connect = function (root) {
+    if (!root) return root
+
+    let queue = [root]
+
+    while (queue.length) {
+        let prev = null
+        let size = queue.length
+
+        for (let i = 0; i < size; i++) {
+            let node = queue.shift()
+
+            if (prev) {
+                prev.next = node
+            }
+            prev = node
+
+            if (node.left) queue.push(node.left)
+            if (node.right) queue.push(node.right)
+
+        }
+
+        prev.next = null
+    }
+
+    return root
+}
