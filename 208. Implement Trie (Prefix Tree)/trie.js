6081940834d4d7ec2f03aa5d99c18fbc13551d38
anubhavkhanna7 <anubhavkhanna7@gmail.com>
2024-10-06 23:55:37 +0530

    More Questions Solved

---
+++ 208. Implement Trie (Prefix Tree)/trie.js
@@ -0,0 +1,61 @@
+class TrieNode {
+  constructor() {
+    this.children = {}; // Each child represents a character
+    this.isEndOfWord = false; // Flag to check if the node marks the end of a word
+  }
+}
+
+var Trie = function() {
+    this.root = new TrieNode();
+};
+
+/** 
+ * @param {string} word
+ * @return {void}
+ */
+Trie.prototype.insert = function(word) {
+    let node = this.root;
+
+    for (let char of word) {
+      if (!node.children[char]) {
+        node.children[char] = new TrieNode();
+      }
+      node = node.children[char];
+    }
+
+    node.isEndOfWord = true;
+};
+
+/** 
+ * @param {string} word
+ * @return {boolean}
+ */
+Trie.prototype.search = function(word) {
+    let node = this.root;
+
+    for (let char of word) {
+      if (!node.children[char]) {
+        return false;
+      }
+      node = node.children[char];
+    }
+
+    return node.isEndOfWord;
+};
+
+/** 
+ * @param {string} prefix
+ * @return {boolean}
+ */
+Trie.prototype.startsWith = function(prefix) {
+    let node = this.root;
+
+    for (let char of prefix) {
+      if (!node.children[char]) {
+        return false;
+      }
+      node = node.children[char];
+    }
+
+    return true;
+};
